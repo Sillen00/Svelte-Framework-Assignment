2050d96ded511ccf7775cb92281bc626e934da63
Simon <sillen.00@hotmail.com>
2023-02-10 14:42:17 +0100

    Celler är utplacerade i rätt färger. centreeing saknas

--- src/App.svelte
+++ src/App.svelte
@@ -11,18 +11,33 @@
 </script>
 
 <main>
+  <h1>State of Javascript</h1>
   {#each frameworks as framework}
     <div class="framework-container framework-container-{framework.name}">
-      <p>{framework.name}</p>
-      <div class="line-container">
-        <div class="line" />
-        <div class="circle-container">
-          {#each framework.surveys as survey}
-            {survey.retention}%
-          {/each}
+      <p class="framework-name-left" style="color: {framework.color};">
+        {framework.name}
+      </p>
+      <!-- <div class="line-container"> -->
+      <div class="line" />
+      <!-- <div class="circle-container"> -->
+
+      {#each framework.surveys as survey}
+        <div class="circle" style="color: {framework.color};">
+          {survey.retention}%
         </div>
-      </div>
-      <p>{framework.name}</p>
+      {/each}
+
+      <!-- {#each framework[0].surveys.length as survey}
+        {#if 7 > framework.surveys.length}
+          <div class="circle" style="color: {framework.color};">%</div>
+        {/if}
+      {/each} -->
+
+      <!-- </div> -->
+      <!-- </div> -->
+      <p class="framework-name-right" style="color: {framework.color};">
+        {framework.name}
+      </p>
     </div>
   {/each}
 
@@ -35,52 +50,55 @@
   <div class="card">
     <Counter />
   </div>
-
-  <h1>Hej {frameworks[0].surveys[0].year}</h1>
 </main>
 
 <style>
+  * {
+    box-sizing: border-box;
+  }
+  h1 {
+    text-align: center;
+    background-color: rgb(20, 20, 20);
+    margin: 0;
+    padding: 1rem;
+  }
+
   .framework-container {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    max-width: 800px;
+    min-width: 500px;
+    margin: 0 auto;
+    border: 1px solid white;
+  }
+
+  .framework-name-left {
+    text-align: right;
+    border: 1px solid orange;
+  }
+
+  .line-container {
+    /* width: 500px; */
+    /* width: 400px; */
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid red;
+  }
+
+  .line {
     position: absolute;
-    left: 50%;
-    transform: translate(-50%);
-    width: 500px;
-    /* color: red; */
+    top: 0;
+    left: 0;
   }
-  .framework-container-React {
-    top: 50px;
+
+  .circle {
+    border: 2px solid red;
+    /* width: 10px; */
   }
-  .framework-container-Vue {
-    top: 100px;
-  }
-  .framework-container-Angular {
-    top: 150px;
-  }
-  .framework-container-Ember {
-    top: 200px;
-  }
-  .framework-container-Preact {
-    top: 250px;
-  }
-  .framework-container-Svelte {
-    top: 300px;
-  }
-  .framework-container-Alpine {
-    top: 350px;
-  }
-  .framework-container-Lit {
-    top: 400px;
-  }
-  .framework-container-Solid {
-    top: 450px;
-  }
-  .framework-container-Qwik {
-    top: 500px;
-  }
-  .framework-container-Stencil {
-    top: 550px;
+
+  .circle-container {
+    border: 1px solid green;
   }
 </style>
